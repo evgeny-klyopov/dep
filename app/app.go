2dcbaeefd01b36b4feb9c9b8134173dd1c4c63cc
Alva <mail@klepov.info>
2020-05-02 02:56:05 +0300

    test update

--- app/app.go
+++ app/app.go
@@ -117,7 +117,7 @@ func NewApp() App {
 }
 
 func (app *App) GetVersion() string {
-	return "v1.0.10"
+	return "v1.0.11"
 }
 func (app *App) HelpTemplate() (appHelp string, commandHelp string) {
 	info := app.Color.White(`{{.Name}} - {{.Usage}}`)
