d7f50954829ae626dde6e595e124095cc8085426
Alva <mail@klepov.info>
2020-05-02 02:28:35 +0300

    fix

--- app/app.go
+++ app/app.go
@@ -117,7 +117,7 @@ func NewApp() App {
 }
 
 func (app *App) GetVersion() string {
-	return "v1.0.8"
+	return "v1.0.9"
 }
 func (app *App) HelpTemplate() (appHelp string, commandHelp string) {
 	info := app.Color.White(`{{.Name}} - {{.Usage}}`)
